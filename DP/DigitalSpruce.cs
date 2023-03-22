52a86445809ad2731e256e480206c3bdf9dcc5ca
Igor Teplych <igorvrt@mail.ru>
2023-03-22 22:49:01 +0600

    Добавил алгоритм "Цифровое дерево"

---
+++ DP/DigitalSpruce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP
+{
+    public class DigitalSpruce
+    {
+        public int GetMaxWay(int[][] tree)
+        {
+            for (int i = tree.Length - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    tree[i][j] = Math.Max(tree[i + 1][j], tree[i + 1][j + 1]) + tree[i][j];
+                }
+            }
+            return tree[0][0];
+        }
+    }
+}
