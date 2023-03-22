b6c5841e3c1055a4bbab399dedc8544d2f639a95
Igor Teplych <igorvrt@mail.ru>
2023-03-22 22:40:34 +0600

    Добавил тестовый проект

---
+++ DP.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DP.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
